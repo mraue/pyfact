00e9bcfa66b145b2e626670126f2db13dd4513d1
Martin Raue <martin.raue@desy.de>
2012-03-05 11:25:08 +0100

    Added numpydoc to doc/npdoc

---
+++ docs/npdoc/__init__.py
@@ -0,0 +1,14 @@
+"""
+numpydoc module from numpy.
+
+References
+----------
+
+Taken from astropy
+https://github.com/astropy/astropy/tree/master/astropy/sphinx/ext
+
+https://github.com/numpy/numpy/blob/master/doc/HOWTO_DOCUMENT.rst.txt
+https://github.com/numpy/numpy/blob/master/doc/sphinxext/numpydoc.py
+
+"""
+from .numpydoc import setup
