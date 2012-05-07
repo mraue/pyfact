172b87a57f237d34e3b3396ef4281c4d9b97d83b
Martin Raue <martin.raue@googlemail.com>
2012-05-07 13:50:17 +0200

    Start 0.2.0dev branch

--- pyfact/__init__.py
+++ pyfact/__init__.py
@@ -49,7 +49,7 @@ The module is splitted in several files according to the functionality for bette
 
 """
 
-__version__ = '0.2.0'
+__version__ = '0.2.0dev'
 __author__  = 'M. Raue // martin.raue@desy.de'
 
 # Import all functions/classes from the different files
